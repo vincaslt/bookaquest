8efcfa0a37e75c23501675d7c17059c9fd736dcd
Vincas Stonys <vincaslt@gmail.com>
2019-09-14 00:03:15 +0300

    Cleanup unused global style

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,19 +3,13 @@ import { hot } from 'react-hot-loader'
 import { Layout, Spin } from 'antd'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 import PrivateRoutes from './PrivateRoutes'
 import PublicRoutes from './PublicRoutes'
 import useUser from './shared/hooks/useUser'
 import { withUserProvider } from './shared/providers/UserProvider'
 import SideMenu from './shared/SideMenu'
 import './utils/i18n'
-
-const GlobalStyles = createGlobalStyle`
-  /* .ant-steps-icon {
-    line-height: inherit !important;
-  } */
-`
 
 const AppSpinnerContainer = styled('div')`
   display: flex;
@@ -70,7 +64,6 @@ function App() {
           <PublicRoutes />
         </PageContainer>
       )}
-      <GlobalStyles />
     </Layout>
   )
 }
